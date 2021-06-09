fac4fcbdc0f46038044ae18adb35abdcf922ae16
GHOST <ghostdevbusiness@gmail.com>
2021-06-09 20:53:26 +0100

    fix: runtime checks

--- src/core/JellyCommands.ts
+++ src/core/JellyCommands.ts
@@ -9,6 +9,14 @@ export class JellyCommands {
         this.client = client;
         this.options = Object.assign(defaultOptions, options);
 
-        if (client instanceof Client) throw new TypeError('Expected a instance of Discord.Client');
+        if (!client)
+            throw new SyntaxError(
+                'Expected a instance of Discord.Client, recieved none',
+            );
+
+        if (client instanceof Client)
+            throw new TypeError(
+                `Expected a instance of Discord.Client, recieved ${typeof client}`,
+            );
     }
 }
