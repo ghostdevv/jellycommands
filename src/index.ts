131c76a4d4593eea6cbec66ec6835f639a632e73
GHOST <ghostdevbusiness@gmail.com>
2021-06-12 16:31:48 +0100

    chore: update exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 // Export lib
 export { JellyCommands } from './core/JellyCommands';
+export { createEvent } from './core/EventManager';
 
 // Export Misc Types
 export type { JellyCommandsOptions } from './options/JellyCommands';
