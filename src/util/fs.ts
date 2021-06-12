a7d15f0b4128fcd64982d61da92f0f4a4c34f72d
GHOST <ghostdevbusiness@gmail.com>
2021-06-12 01:53:32 +0100

    feat: readdirRecursiveSync

---
+++ src/util/fs.ts
@@ -0,0 +1,15 @@
+import { readdirSync, lstatSync } from 'fs';
+import { join } from 'path';
+
+export const readdirRecursiveSync = (path: string): string[] =>
+    readdirSync(path)
+        .map((file) => join(path, file))
+        .reduce(
+            (files: string[], file: string) => [
+                ...files,
+                ...(lstatSync(file).isDirectory()
+                    ? readdirRecursiveSync(file)
+                    : [file]),
+            ],
+            [],
+        );
