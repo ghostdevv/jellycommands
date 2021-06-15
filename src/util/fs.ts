becf1d8a19761db2b75ab0fd501e7d4bb884df1c
GHOST <ghostdevbusiness@gmail.com>
2021-06-15 23:06:34 +0100

    fix: fs

--- src/util/fs.ts
+++ src/util/fs.ts
@@ -20,10 +20,11 @@ export const readdirRecursiveSync = (path: string): string[] =>
 
 // If there is a default it returns it, if there is a default but other properties it strips the default
 export const resolveImport = (imp: { default?: any }) => {
+    imp = Object.assign({}, imp);
+
     if (imp.default && Object.keys(imp).length == 1) return imp.default;
 
     delete imp.default;
-
     return imp;
 };
 
