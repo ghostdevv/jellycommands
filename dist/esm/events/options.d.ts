38f82459ba75b61941adeec41b0308ab3627bff0
GHOST <ghostdevbusiness@gmail.com>
2021-06-15 23:26:34 +0100

    chore: update building

--- dist/esm/events/options.d.ts
+++
@@ -1,8 +0,0 @@
-import type { ClientEvents } from 'discord.js';
-export declare const defaults: {
-    name: keyof ClientEvents;
-    disabled: boolean;
-    once: boolean;
-};
-import Joi from 'joi';
-export declare const schema: Joi.ObjectSchema<any>;
