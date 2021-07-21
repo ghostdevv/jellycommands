4ada933582601fe4649899a979ae77836c888fb7
GHOST <ghostdevbusiness@gmail.com>
2021-07-21 12:24:14 +0100

    feat: allowDM option

--- src/JellyCommands/commands/options.ts
+++ src/JellyCommands/commands/options.ts
@@ -3,6 +3,11 @@ export const defaults = {
      * Whether or not the command should be loaded
      */
     disabled: false,
+
+    /**
+     * Should the command work in dms
+     */
+    allowDM: false,
 };
 
 import Joi from 'joi';
