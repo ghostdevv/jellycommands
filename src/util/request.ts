0527684bb646a4bbec9082c32a00a53a595c635d
GHOST <ghostdevbusiness@gmail.com>
2021-09-03 17:29:01 +0100

    feat: include retry_after

--- src/util/request.ts
+++ src/util/request.ts
@@ -14,7 +14,7 @@ export function createRequest(token: string) {
             .then((res) => res.data as T)
             .catch((e: AxiosError) => {
                 const error = e.response?.data?.message
-                    ? `[DISCORD API ERROR] ${e.response?.data?.message}`
+                    ? `[DISCORD API ERROR] ${e.response.data.message} - Try again in ${e.response.data.retry_after}s`
                     : e.message;
 
                 throw new Error(error);
