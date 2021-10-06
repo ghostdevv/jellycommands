afc622e75784421b21692fbe09ce082569eb08dc
GHOST <ghostdevbusiness@gmail.com>
2021-10-06 22:36:06 +0100

    fix: request

--- src/util/request.ts
+++ src/util/request.ts
@@ -12,9 +12,15 @@ export function createRequest(token: string) {
     return <T>(method: Method, route: string, data?: any) =>
         req(route, { method, data })
             .then((res) => res.data as T)
-            .catch((e: AxiosError) => {
-                const error = e.response?.data?.message
-                    ? `[DISCORD API ERROR] ${e.response.data.message} - Try again in ${e.response.data.retry_after}s`
+            .catch((e) => {
+                if (!axios.isAxiosError(e)) return e;
+
+                const { message } = (e?.response?.data || {}) as {
+                    message?: string;
+                };
+
+                const error = message
+                    ? `[DISCORD API ERROR] ${message} - Try again in ${message}s`
                     : e.message;
 
                 throw new Error(error);
