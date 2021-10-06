dd84ce599572b1d4b9e8addd3d7d6ede84876ab8
GHOST <ghostdevbusiness@gmail.com>
2021-10-07 00:35:19 +0100

    fix: name rule

--- src/JellyCommands/commands/types/commands/options.ts
+++ src/JellyCommands/commands/types/commands/options.ts
@@ -16,6 +16,19 @@ export interface CommandOptions extends BaseOptions {
 import Joi from 'joi';
 
 export const schema = baseSchema.append({
+    // Enforce good registration rule
+    name: Joi.string()
+        .required()
+        .prefs({ convert: false })
+        .ruleset.lowercase()
+        .min(1)
+        .max(32)
+        .pattern(/^[a-z0-9]+$/)
+        .rule({
+            message:
+                'Slash Command names must be 1 - 32 characters, all lowercase with no witespaces or special chars',
+        }),
+
     description: Joi.string().required(),
 
     options: Joi.array(),
