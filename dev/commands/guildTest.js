6187de16e5a439a077b31c6e2b1b83bed420466d
GHOST <ghostdevbusiness@gmail.com>
2021-10-08 00:03:17 +0100

    fix: command

--- dev/commands/guildTest.js
+++ dev/commands/guildTest.js
@@ -4,18 +4,17 @@ export default command({
     name: 'guildtest',
     description: 'A testing command bound to guild not global',
 
-    // guilds: ['663140687591768074'],
-    global: true,
+    guilds: ['663140687591768074'],
 
     dev: true,
 
-    // guards: {
-    //     mode: 'blacklist',
+    guards: {
+        mode: 'blacklist',
 
-    //     users: ['331151683067445251'],
+        users: ['331151683067445251'],
 
-    //     roles: ['740589901095108739'],
-    // },
+        roles: ['740589901095108739'],
+    },
 
     run: ({ interaction }) =>
         interaction.reply({ embeds: [{ description: 'as' }] }),
