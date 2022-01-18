9bde1e6bcb35fd53dd2e42e81b83156b57289988
GHOST <ghostdevbusiness@gmail.com>
2022-01-18 11:28:19 +0000

    chore: remove old structures

--- packages/jellycommands/src/types/commands.d.ts
+++
@@ -1,8 +0,0 @@
-import type { BaseCommand } from '../JellyCommands/commands/base/BaseCommand';
-import type { BaseOptions } from '../JellyCommands/commands/base/options';
-
-export type commandsList = Set<BaseCommand<BaseOptions>>;
-export type commandIdMap = Map<string, BaseCommand<BaseOptions>>;
-
-export type guildCommands = Map<string, commandsList>;
-export type globalCommands = commandsList;
