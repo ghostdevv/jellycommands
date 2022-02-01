76ad82d52adf3e56ec1a48505a44c339a6838f28
GHOSt <ghostdevbusiness@gmail.com>
2022-02-01 19:32:51 +0000

    fix: remove unused export

--- packages/jellycommands/src/index.ts
+++ packages/jellycommands/src/index.ts
@@ -20,6 +20,3 @@ export type { UserCommandOptions } from './JellyCommands/commands/types/userComm
 // Export event related
 export { event, Event } from './JellyCommands/events/Event';
 export type { EventOptions } from './JellyCommands/events/options';
-
-// Export helpers
-export {loadEvents, loadCommands} from "./util/loaders.js";
