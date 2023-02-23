1c210eda87d188355eabb13d876fc37226980bcc
Cain <75994858+cainthebest@users.noreply.github.com>
2023-02-24 00:03:54 +0100

    impl(test): clientIdFromToken

--- packages/jellycommands/tests/unit/utils/token.test.ts
+++ packages/jellycommands/tests/unit/utils/token.test.ts
@@ -1,7 +1,14 @@
 import assert from 'assert';
 import { test } from 'uvu';
 
-import { cleanToken } from '../../../src/utils/token';
+import {
+    cleanToken,
+    clientIdFromToken,
+    resolveToken,
+    resolveClientId,
+    getAuthData,
+    type AuthData,
+} from '../../../src/utils/token';
 
 test('Clean a potential token', () => {
     // Insure a string is returned and the `Bot` is removed
@@ -25,6 +32,28 @@ test('Clean a potential token', () => {
     // @ts-ignore
     const notString = cleanToken(1234567890);
     assert.strictEqual(notString, null);
+});
+
+test('clientIdFromToken should return the client ID from the token', () => {
+    // This is not a real token, it is just a base64 encoded string
+    // that follows the format of a token
+    const token = 'MTIzNDU2Nzg5MA==.dGVzdENsaWVudElk';
+    const clientId = clientIdFromToken(token);
+    const expectedClientId = '1234567890';
+    assert.strictEqual(clientId, expectedClientId);
+
+    // If the token is not valid format, null is returned
+    // The client ID is one of 2 parts of the token,
+    // Both parts are seperated by a `.`
+    const InvalidString = clientIdFromToken('MTIzNDU2Nzg5MA==');
+    assert.strictEqual(InvalidString, null);
+
+    // If the token is not a string, null is returned
+    //! https://github.com/ghostdevv/jellycommands/issues/167
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    //const notString = clientIdFromToken(1234567890);
+    //assert.strictEqual(notString, null);
 });
 
 test.run();
