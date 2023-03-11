6375b26055b090d86a9b99603ff8e0d3d5573257
GHOST <ghostdevbusiness@gmail.com>
2023-03-11 19:37:39 +0000

    feat(dev): add test button

---
+++ dev/src/commands/file-loaded/testButton.js
@@ -0,0 +1,21 @@
+import { ButtonBuilder, ActionRowBuilder, ButtonStyle } from 'discord.js';
+import { command } from 'jellycommands';
+
+export default command({
+    name: 'testbutton',
+    description: 'Test the buttons',
+
+    global: true,
+    defer: true,
+
+    async run({ interaction }) {
+        const row = new ActionRowBuilder().addComponents(
+            new ButtonBuilder().setCustomId('test').setLabel('test').setStyle(ButtonStyle.Primary),
+        );
+
+        interaction.followUp({
+            content: 'Test Buttons',
+            components: [row],
+        });
+    },
+});
