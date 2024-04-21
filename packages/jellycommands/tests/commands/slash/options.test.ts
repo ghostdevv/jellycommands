64a39ffe3ed7c6369d76ff8098f688ac147d950d
GHOST <ghostdevbusiness@gmail.com>
2024-04-21 19:51:16 +0100

    tests: more coverage on command options

--- packages/jellycommands/tests/commands/slash/options.test.ts
+++ packages/jellycommands/tests/commands/slash/options.test.ts
@@ -10,8 +10,49 @@ describe('command options schema', () => {
     });
 
     it('rejects invalid names', () => {
-        expect(() =>
-            commandSchema.parse({ name: 'NAME', description: 'descriptive' }),
-        ).toThrowError();
+        const description = 'descriptive';
+
+        expect(() => commandSchema.parse({ name: 'NAME', description })).toThrowError();
+        expect(() => commandSchema.parse({ name: 'spaces are wrong', description })).toThrowError();
+        expect(() => commandSchema.parse({ name: 'no'.repeat(200), description })).toThrowError();
+        expect(() => commandSchema.parse({ name: 'a'.repeat(33), description })).toThrowError();
+    });
+
+    it('rejects invalid descriptions', () => {
+        const name = 'name';
+
+        expect(() => commandSchema.parse({ name })).toThrowError();
+        expect(() => commandSchema.parse({ name, description: 'no'.repeat(200) })).toThrowError();
+        expect(() => commandSchema.parse({ name, description: 'a'.repeat(101) })).toThrowError();
+    });
+
+    it('supports description localisations', () => {
+        const result = parseSchema('command', commandSchema, {
+            name: 'name',
+            description: 'descriptive',
+            descriptionLocalizations: {
+                'en-gb': 'it works',
+            },
+        });
+
+        expect(result.descriptionLocalizations?.['en-gb']).toBe('it works');
+    });
+
+    it('supports options', () => {
+        const result = parseSchema('command', commandSchema, {
+            name: 'name',
+            description: 'descriptive',
+            options: [
+                {
+                    type: 'Channel',
+                    name: 'channel',
+                    description: 'channel to use',
+                    required: true,
+                },
+            ],
+        });
+
+        expect(result.options).toHaveLength(1);
+        expect(result.options?.[0]?.name).toBe('channel');
     });
 });
