6eda0138ca7ac9fda52d8ccdce1339925902ded4
Brooke <46959407+nimajnebec@users.noreply.github.com>
2024-11-15 22:39:47 +0000

    feat: add modal component

---
+++ packages/jellycommands/src/components/modals/options.ts
@@ -0,0 +1,40 @@
+import type { InteractionDeferReplyOptions } from 'discord.js';
+import type { BaseComponentOptions } from '../components';
+import type { MaybePromise } from '../../utils/types';
+import { z } from 'zod';
+
+export interface ModalOptions extends BaseComponentOptions {
+	/**
+	 * The customId of the modal, or a regex/function to match against
+	 */
+	id: string | RegExp | ((id: string) => MaybePromise<boolean>);
+
+	/**
+	 * Should the interaction be defered?
+	 */
+	defer?: boolean | InteractionDeferReplyOptions;
+}
+
+export const modalSchema = z.object({
+	id: z.union([
+		z.string(),
+		z.instanceof(RegExp),
+		// todo test this
+		z
+			.function()
+			.args(z.string().optional())
+			.returns(z.union([z.boolean(), z.promise(z.boolean())])),
+	]),
+
+	defer: z
+		.union([
+			z.boolean().default(false),
+			z.object({
+				ephemeral: z.boolean().optional(),
+				fetchReply: z.boolean().optional(),
+			}),
+		])
+		.optional(),
+
+	disabled: z.boolean().default(false).optional(),
+});
