6b2a56bf89f8fbfbedb765cb2bad3c7303c91995
Willow (GHOST) <git@willow.sh>
2025-10-18 14:21:11 +0100

    deps: remove tsm and desm

--- packages/jellycommands/vitest.config.ts
+++ packages/jellycommands/vitest.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vitest/config';
-import { join } from 'desm';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
 	test: {
 		alias: {
-			$src: join(import.meta.url, './src'),
-			$mock: join(import.meta.url, './tests/mock.ts'),
+			$src: join(__dirname, './src'),
+			$mock: join(__dirname, './tests/mock.ts'),
 		},
 		include: ['tests/**/*.test.ts'],
 		unstubEnvs: true,
